a19b9ebc2b04ce270fa635cf619a67485cc1ecaa
Ahmed-Emad2 <sci_223667@omu.edu.ly>
2024-08-17 12:01:07 +0200

    Add files via upload

---
+++ HW02/HW02.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+
+{
+string name ;
+int num ;
+
+cout << "inter the name" << endl;
+cin >>name ;
+
+cout<<" inter the Academic number\n";
+cin >>num;
+
+cout<<name<<endl;
+cout<<num;
+}
